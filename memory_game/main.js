b8bfe1ebb5557ace329541fa5969f3bfb54ae976
Sammy <asfernandes@sbcglobal.net>
2019-05-10 11:26:22 -0400

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,11 @@
+console.log("up and running");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree ="king";
+var cardFour = "king";
+
+console.log("User Flipped " + cardFour);
+
+console.log("User Flipped " + cardThree);
+
