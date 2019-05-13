8115caa498a083a9a325bbb5caa4f7b9845e1438
Sammy <asfernandes@sbcglobal.net>
2019-05-13 11:30:05 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,11 +1,23 @@
-console.log("up and running");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree ="king";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console.log("User Flipped " + cardFour);
+var cardOne = cards[0];
 
-console.log("User Flipped " + cardThree);
+cardsInPlay.push(cardOne);
 
+console.log("user flipped " + cardOne);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped " + cardTwo);
+
+if(cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!")
+	} else {
+		alert("sorry, try again.")
+	}
+}
